9f86c6afae53dbc2f6ed1e02707e9ef89c02aee1
Kirtiman Singh <kirt93@hotmail.com>
2018-09-10 14:50:32 +0100

    transactions array of hashes with date,credit,debit and balance

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,14 +1,20 @@
+require 'date'
+
 class Bank
-  attr_reader :balance
+  attr_reader :balance, :transactions
   def initialize
     @balance = 0
+    @transactions = []
   end
 
   def deposit(amount)
     @balance =+ amount
+    @transactions.push(date: Date.today, credit:amount, debit:0, balance: @balance)
   end
 
   def withdraw(amount)
+    fail 'Insufficient Funds' if amount > @balance
     @balance -= amount
+    @transactions.push(date: Date.today, credit:0, debit:amount, balance: @balance)
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,23 +1,33 @@
 require 'bank'
 
-describe 'Bank Account' do
-  # subject(:bank_account){described_class.new}
-  subject(:bank_account) { Bank.new }
-  # bank_account = Bank.new
-  it 'has an initial balance of 0' do
-    expect(subject.balance).to eq(0)
-  end
+describe Bank do
+  subject(:account) {described_class.new}
 
-  it 'can deposit money' do
-    # expect((subject.deposit(1000)).balance).to eq(1000)
-    subject.deposit(1000)
-    expect(subject.balance).to eq(1000)
-  end
+  describe 'Bank Account:' do
+    it 'has an initial balance of 0' do
+      expect(subject.balance).to eq(0)
+    end
 
-  it 'can withdraw money' do
-    subject.deposit(3000)
-    subject.withdraw(500)
-    expect(subject.balance).to eq(2500)
-  end
+    it 'can credit amount' do
+      # expect((subject.deposit(1000)).balance).to eq(1000)
+      subject.deposit(1000)
+      expect(subject.balance).to eq(1000)
+    end
 
+    it 'can debit amount' do
+      subject.deposit(3000)
+      subject.withdraw(500)
+      expect(subject.balance).to eq(2500)
+    end
+
+    it 'cannot withdraw if debit amount is more than balance' do
+      expect { subject.withdraw(100) }.to raise_error 'Insufficient Funds'
+    end
+
+    it 'can show transaction history' do
+      subject.deposit(3000)
+      subject.withdraw(500)
+      expect(subject.transactions).to include({ date: Date.today, credit:3000, debit:0, balance:3000 }, { date: Date.today, credit:0, debit:500, balance:2500 })
+    end
+  end
 end
