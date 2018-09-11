4ba57e3f0c6fbf6620c0905a4e864385b47bf681
Kirtiman Singh <kirt93@hotmail.com>
2018-09-11 18:44:33 +0100

    README updated and tests edited and organised

--- lib/bank.rb
+++ lib/bank.rb
@@ -11,14 +11,14 @@ class Bank
     fail 'Cannot credit a nil or a negative amount' if amount <= 0
 
     @balance += amount
-    @transactions.push(date: Time.now.strftime("%d/%m/%Y"), credit: amount, debit: 0, balance: @balance)
+    @transactions.push(date: Time.now.strftime("%d/%m/%Y"), credit: sprintf('%.2f', amount), debit: nil, balance: sprintf('%.2f', @balance))
   end
 
   def withdraw(amount)
     fail 'Insufficient Funds' if amount > @balance
 
     @balance -= amount
-    @transactions.push(date: Time.now.strftime("%d/%m/%Y"), credit: 0, debit: amount, balance: @balance)
+    @transactions.push(date: Time.now.strftime("%d/%m/%Y"), credit: nil, debit: sprintf('%.2f', amount), balance: sprintf('%.2f', @balance))
   end
 
   def statement
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,47 +4,55 @@ describe Bank do
   subject(:account) { described_class.new }
 
   describe 'Bank Account:' do
-    it 'has an initial balance of 0' do
-      expect(subject.balance).to eq(0)
+    describe ':balance' do
+      it 'has an initial balance of 0' do
+        expect(subject.balance).to eq(0)
+      end
     end
 
-    it 'can credit amount' do
-      # expect((subject.deposit(1000)).balance).to eq(1000)
-      subject.deposit(1000)
-      subject.deposit(1000)
-      expect(subject.balance).to eq(2000)
+    describe "#deposit" do
+      it 'can credit amount' do
+        subject.deposit(1000)
+        subject.deposit(1000)
+        expect(subject.balance).to eq(2000)
+      end
+      it 'cannot credit a negative amount' do
+        expect { subject.deposit(-10) }.to raise_error 'Cannot credit a nil or a negative amount'
+      end
     end
 
-    it 'cannot credit a negative amount' do
-      expect { subject.deposit(-10) }.to raise_error 'Cannot credit a nil or a negative amount'
+    describe "#withdraw" do
+      it 'can debit amount' do
+        subject.deposit(3000)
+        subject.withdraw(500)
+        subject.withdraw(500)
+        expect(subject.balance).to eq(2000)
+      end
+
+      it 'cannot withdraw if debit amount is more than balance' do
+        expect { subject.withdraw(100) }.to raise_error 'Insufficient Funds'
+      end
     end
 
-    it 'can debit amount' do
-      subject.deposit(3000)
-      subject.withdraw(500)
-      subject.withdraw(500)
-      expect(subject.balance).to eq(2000)
+    describe ':transactions' do
+      it 'initially has no transactions' do
+        expect(subject.transactions).to be_empty
+      end
+      it 'can show transaction history' do
+        subject.deposit(3000)
+        subject.withdraw(500)
+        expect(subject.transactions).to include({ date: Time.now.strftime("%d/%m/%Y"),
+        credit: "3000.00", debit: nil, balance: "3000.00" }, { date: Time.now.strftime("%d/%m/%Y"),
+        credit: nil, debit: "500.00", balance: "2500.00" })
+      end
     end
 
-    it 'cannot withdraw if debit amount is more than balance' do
-      expect { subject.withdraw(100) }.to raise_error 'Insufficient Funds'
+    describe "#statement" do
+      it 'statement shows date, credit amount, debit amount and balance' do
+        subject.deposit(3000)
+        expect { subject.statement }.to output("date || credit || debit || balance \n#{Time.now.strftime('%d/%m/%Y')} || 3000.00 ||  || 3000.00\n").to_stdout
+      end
     end
 
-    it 'initially has no transactions' do
-      expect(subject.transactions).to be_empty
-    end
-
-    it 'can show transaction history' do
-      subject.deposit(3000)
-      subject.withdraw(500)
-      expect(subject.transactions).to include({ date: Time.now.strftime("%d/%m/%Y"),
-      credit: 3000, debit: 0, balance: 3000 }, { date: Time.now.strftime("%d/%m/%Y"),
-      credit: 0, debit: 500, balance: 2500 })
-    end
-
-    it 'statement shows date, credit amount, debit amount and balance' do
-      subject.deposit(3000)
-      expect { subject.statement }.to output("date || credit || debit || balance \n#{Time.now.strftime('%d/%m/%Y')} || 3000 || 0 || 3000\n").to_stdout
-    end
   end
 end
